15410f01c033f611b2a4fa565dab16ba33bf67e7
sumankum <sumankum@INsumankum.local>
2015-06-08 11:47:04 +0530

    Added spec to check if life is getting assigned to cell

--- src/Grid.java
+++ src/Grid.java
@@ -1,6 +1,6 @@
 public class Grid {
 
-    int gridBox[][];
+    public final int gridBox[][];
 
     public Grid(int size) {
         gridBox = new int[size][size];
@@ -8,5 +8,9 @@ public class Grid {
 
     public int getAliveMembers(int x, int y) {
         return (gridBox[x - 1][y + 1] + gridBox[x - 1][y] + gridBox[x - 1][y - 1] + gridBox[x][y + 1] + gridBox[x][y - 1] + gridBox[x + 1][y - 1] + gridBox[x + 1][y] + gridBox[x + 1][y + 1]);
+    }
+
+    public void assignLifeToCell(int x, int y, int life) {
+        gridBox[x][y] = life;
     }
 }
--- test/GridTest.java
+++ test/GridTest.java
@@ -1,15 +1,28 @@
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class GridTest {
     @Test
-    public void checkIfNeighboursPresentAreAllDead() {
+    public void checkIfNeighboursPresentAreAllDead(){
         Grid grid = new Grid(50);
 
-        int actualDeadMembers = grid.getAliveMembers(5, 5);
+        int actualDeadMembers=grid.getAliveMembers(5,5);
         int expectedDeadMembers = 0;
 
         assertEquals(actualDeadMembers, expectedDeadMembers);
+    }
+
+    @Test
+    public void checkIfLifeAssignedToCell(){
+        Grid grid=new Grid(50);
+        grid.assignLifeToCell(5,5,1);
+
+        int actualLife = grid.gridBox[5][5];
+        int expectedLife = 1;
+        
+        assertThat(actualLife, is(expectedLife));
     }
 }
